9c8896d100356076610d3b30fa5706872800c274
achamaro <a.achamaro@gmail.com>
2025-01-11 22:19:10 +0900

    chore: Honoサーバー

--- vite.config.ts
+++ vite.config.ts
@@ -1,49 +1,40 @@
-import { vitePluginViteNodeMiniflare } from "@hiogawa/vite-node-miniflare";
+import adapter from "@hono/vite-dev-server/cloudflare";
 import { reactRouter } from "@react-router/dev/vite";
 import autoprefixer from "autoprefixer";
+import serverAdapter from "hono-react-router-adapter/vite";
 import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(({ isSsrBuild }) => ({
-  build: {
-    rollupOptions: isSsrBuild
-      ? {
-          input: "./workers/app.ts",
-        }
-      : undefined,
-  },
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
-  },
-  ssr: {
-    target: "webworker",
-    noExternal: true,
-    resolve: {
-      conditions: ["workerd", "browser"],
-    },
-    optimizeDeps: {
-      include: [
-        "react",
-        "react/jsx-runtime",
-        "react/jsx-dev-runtime",
-        "react-dom",
-        "react-dom/server",
-        "react-router",
-      ],
-    },
-  },
-  plugins: [
-    vitePluginViteNodeMiniflare({
-      entry: "./workers/app.ts",
-      miniflareOptions: (options) => {
-        options.compatibilityDate = "2024-11-18";
-        options.compatibilityFlags = ["nodejs_compat"];
-      },
-    }),
-    reactRouter(),
-    tsconfigPaths(),
-  ],
+export default defineConfig((_) => ({
+	css: {
+		postcss: {
+			plugins: [tailwindcss, autoprefixer],
+		},
+	},
+	ssr: {
+		target: "webworker",
+		noExternal: true,
+		resolve: {
+			conditions: ["workerd", "browser"],
+		},
+		optimizeDeps: {
+			include: [
+				"react",
+				"react/jsx-runtime",
+				"react/jsx-dev-runtime",
+				"react-dom",
+				"react-dom/server",
+				"react-router",
+			],
+		},
+	},
+	plugins: [
+		reactRouter(),
+		serverAdapter({
+			adapter,
+			entry: "server/index.ts",
+		}),
+		tsconfigPaths(),
+	],
 }));
